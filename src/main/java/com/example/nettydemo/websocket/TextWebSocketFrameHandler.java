e860950018949792536f8cef6b20aaba6a56080f
zhaoxueke1 <zhaoxueke1@tal.com>
2021-09-09 14:37:15 +0800

    update

--- src/main/java/com/example/nettydemo/websocket/TextWebSocketFrameHandler.java
+++ src/main/java/com/example/nettydemo/websocket/TextWebSocketFrameHandler.java
@@ -3,8 +3,6 @@ package com.example.nettydemo.websocket;
 import com.example.nettydemo.exception.FormatException;
 import com.example.nettydemo.utils.JsonUtil;
 import com.example.nettydemo.utils.RequestUriUtils;
-import com.google.common.collect.ImmutableMap;
-import com.google.common.collect.Maps;
 import io.netty.channel.Channel;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
@@ -18,7 +16,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 /**
